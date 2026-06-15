ebaddaea9dda05556ebc1c06f4f53d019db69f59
Yildiz Kurt <y.kurt@rocva.nl>
2026-06-15 15:36:06 +0200

    Create: formulier en store

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+    $games = Game::all();
+    return view('games.index', compact('games'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('games.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+    $request->validate([
+        'game_name' => 'required',
+        'platform' => 'required',
+        'genre' => 'required',
+        'rating' => 'required|numeric|min:0|max:10'
+    ]);
+
+    $game = new Game([
+        'game_name' => $request->get('game_name'),
+        'platform' => $request->get('platform'),
+        'genre' => $request->get('genre'),
+        'rating' => $request->get('rating')
+    ]);
+
+    $game->save();
+
+    return redirect('/games')->with('success', 'Game added!');
+}
+
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
+
+Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
+
+Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
