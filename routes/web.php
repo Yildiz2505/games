f867e8501a8e752a5db69d0686f0725cf76c0650
Yildiz Kurt <y.kurt@rocva.nl>
2026-06-23 15:05:35 +0200

    Les 3: Middleware: pagina’s beschermen + games collectie

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,30 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
+
+Route::get('/', function () {    return view('welcome');});
+
+Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
+
+Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
+Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
+
+Route::get('games/edit/{id}', [App\Http\Controllers\GameController::class, 'edit']);
+Route::post('games/update/{id}', [App\Http\Controllers\GameController::class, 'update']);
+
+Route::post('games/destroy/{id}', [App\Http\Controllers\GameController::class, 'destroy']);
+
+Route::get('/geheim', function () {    return view('geheim');})->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/geheim', function () {
-    return view('geheim');
-})->middleware('auth');
 
 require __DIR__.'/auth.php';
