4bd29c39441f2392f24388780e2335030809df98
Yildiz Kurt <y.kurt@rocva.nl>
2026-06-23 16:06:55 +0200

    Les 4: Game Collection achter een login

--- routes/web.php
+++ routes/web.php
@@ -4,29 +4,30 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    });
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
 });
 
-Route::get('/', function () {    return view('welcome');});
 
-Route::get('games', [App\Http\Controllers\GameController::class, 'index']);
+Route::get('/geheim', function () {    return view('geheim');})->middleware('auth');
 
 Route::get('games/create', [App\Http\Controllers\GameController::class, 'create']);
 Route::post('games/store', [App\Http\Controllers\GameController::class, 'store']);
-
-Route::get('games/edit/{id}', [App\Http\Controllers\GameController::class, 'edit']);
 Route::post('games/update/{id}', [App\Http\Controllers\GameController::class, 'update']);
-
+Route::get('games/edit/{id}', [App\Http\Controllers\GameController::class, 'edit']);
 Route::post('games/destroy/{id}', [App\Http\Controllers\GameController::class, 'destroy']);
-
-Route::get('/geheim', function () {    return view('geheim');})->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/', function () {    return view('welcome');});
+
 
 require __DIR__.'/auth.php';
